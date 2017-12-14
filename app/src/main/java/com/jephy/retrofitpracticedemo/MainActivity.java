fc64e384f524c5a4ba497a4aa211934992569c54
jfyang <jfyang@evomotion.com>
2017-12-14 16:20:17 +0800

    optimize imports

--- app/src/main/java/com/jephy/retrofitpracticedemo/MainActivity.java
+++ app/src/main/java/com/jephy/retrofitpracticedemo/MainActivity.java
@@ -1,9 +1,8 @@
 package com.jephy.retrofitpracticedemo;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,21 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-import java.io.IOException;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements FirmwareUpdateView{
     private static final String TAG = "MyMainActivity";
