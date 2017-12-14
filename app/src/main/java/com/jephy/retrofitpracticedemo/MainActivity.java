00c6b9c74f41cf43d0d61324183864cd5c1d7dd6
jfyang <jfyang@evomotion.com>
2017-12-14 15:37:33 +0800

    add recycler view

--- app/src/main/java/com/jephy/retrofitpracticedemo/MainActivity.java
+++ app/src/main/java/com/jephy/retrofitpracticedemo/MainActivity.java
@@ -1,9 +1,15 @@
 package com.jephy.retrofitpracticedemo;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,12 +23,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyMainActivity";
     private static final String requestURL = "http://192.168.0.77:100/index/updateInfoAll";
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        requestFirmwareInfoAsync();
+        recyclerView = findViewById(R.id.recycler_list_view);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new MyAdapter(MainActivity.this));
+
         requestFirmwareInfoSync();
     }
 
@@ -41,10 +54,10 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<AllUpdateInfoResponse> call, Response<AllUpdateInfoResponse> response) {
                 Log.d(TAG, "response = " + response);
                 int error = response.body().getError();
-                List<AllUpdateInfoResponse.VersionModel> versionModelList = response.body().getData();
+                List<FirmwareVersionModel> firmwareVersionModelList = response.body().getData();
 
                 Log.d(TAG, "error = " + error);
-                Log.d(TAG, "versionModelList = " + versionModelList);
+                Log.d(TAG, "versionModelList = " + firmwareVersionModelList);
             }
 
             @Override
@@ -80,4 +93,42 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
+    private class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        private  LayoutInflater mLayoutInflater;
+        private Context mContext;
+        private List<FirmwareVersionModel> firmwareVersionModelList;
+
+        public MyAdapter(Context mContext) {
+            this.mContext = mContext;
+            this.mLayoutInflater = LayoutInflater.from(mContext);
+            this.firmwareVersionModelList = firmwareVersionModelList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.firmware_info_item,parent,false);
+            MyViewHolder myViewHolder = new MyViewHolder(view);
+
+            return myViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 2;
+        }
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
